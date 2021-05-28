5570e986b1b9f5c4320d4df578355a1840b052bc
zeynep-dmrl <56252190+zeynep-dmrl@users.noreply.github.com>
2021-05-28 19:00:44 +0300

    Update handler.js

--- Olasılık_Web_projesi/handler.js
+++ Olasılık_Web_projesi/handler.js
@@ -1,49 +1,50 @@
 let veri = [5,11,22,5,6,4,654,76,76,4,32,55,776,986,4,54,34,5,11,22,5,6,4,654,4,32,55,776];
 $(document).ready(function(){
 
-    $("#selectMod").on("click",function(){
-        //$("#mod").toggle();
-        $('#mod').show();
+     $("#selectMod").on("click",function(){
+        $("#mod").fadeToggle(); #sure eklenebiliyor parantez içine
+        //$('#mod').show();
+    
         $('#modsonuc').text('Sonuc:' + modHesapla(veri));
     });
 
     $("#selectMedyan").on("click",function(){
-        $("#medyan").show();
+        $("#medyan").fadeToggle();
         $('#medyansonuc').text('Sonuç: ' + medyanHesapla(veri));
     });
 
     $("#selectSapma").on("click",function(){
-        $("#sapma").show();
+        $("#sapma").fadeToggle();
         $("#sapmasonuc").text('Sonuç: ' + standartSapmaHesapla(veri) + ', yaklaşık:' + Math.round(standartSapmaHesapla(veri)));
     });
 
     $("#selectVaryans").on("click",function(){
-        $("#varyans").show();
+        $("#varyans").fadeToggle();
         $("#varyanssonuc").text('Sonuç: ' + varyansHesapla(veri) + ', yaklaşık: ' + Math.round(varyansHesapla(veri)));
     });
 
     $("#selectAOrt").on("click",function(){
-        $("#aort").show();
+        $("#aort").fadeToggle();
         $("#aortsonuc").text('Sonuç: ' + aritmetikOrtHesapla(veri) + ', yaklaşık: ' + Math.round(aritmetikOrtHesapla(veri)));
     });
 
     $("#selectHOrt").on("click",function(){
-        $("#hort").show();
+        $("#hort").fadeToggle();
         $('#hortsonuc').text('Sonuç: ' + harmonikOrtHesapla(veri) + ', yaklaşık: ' + Math.round(harmonikOrtHesapla(veri)));
     });
 
     $("#selectGOrt").on("click",function(){
-        $("#gort").show();
+        $("#gort").fadeToggle();
         $("#gortsonuc").text('Sonuç: ' + geometrikOrtHesapla(veri) + ', yaklaşık: ' + Math.round(geometrikOrtHesapla(veri)));
     });
 
     $("#selectOSapma").on("click",function(){
-        $("#osapma").show();
+        $("#osapma").fadeToggle();
         $('#osapmasonuc').text('Sonuç: ' + ortalamaMutlakSapmaHesapla(veri) + ', yaklaşık: ' + Math.round(ortalamaMutlakSapmaHesapla(veri)));
     });
     
     $("#selectDKat").on("click",function(){        
-        $("#dkatsayısı").show();
+        $("#dkatsayısı").fadeToggle();
         $("#dkatsayısısonuc").text('Sonuç: ' + degiskenKatsayisiHesapla(veri) + ', yaklaşık: ' + Math.round( degiskenKatsayisiHesapla(veri)));
     });
 
